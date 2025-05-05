ec0c8124be673b19659f1d01aa1a832e2ed4c3c0
MejriZiad <mejri.ziad@gmail.com>
2025-05-05 15:46:33 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
+const CACHE_NAME = 'quiz-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles/main.css',
+  '/scripts/main.js',
+  '/assets/icons/*',
+  '/assets/sounds/*',
+  '/assets/images/*'
+];
 
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
